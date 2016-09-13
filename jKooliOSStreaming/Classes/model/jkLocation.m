79733cd98a50e20e10e08d838422bdebaaf3cfc7
cbdbeb9 <catherinebernardone@catherines-mac-mini.local>
2016-09-13 15:26:29 -0400

    Kool locationing objects added.

---
+++ jKooliOSStreaming/Classes/model/jkLocation.m
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2010 jKool, LLC. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * jKool, LLC. ("Confidential Information").  You shall not disclose
+ * such Confidential Information and shall use it only in accordance with
+ * the terms of the license agreement you entered into with jKool, LLC.
+ *
+ * JKOOL MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. JKOOL SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ * CopyrightVersion 1.0
+ *
+ */
+
+#import "jkLocation.h"
+
+
+@implementation jkLocation
+
+@synthesize detectedLocation = _detectedLocation;
+
+
+- (void) kickOffLocationing {
+
+    CLLocationManager *locationManager = [[CLLocationManager alloc] init];
+    if ([locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+        [locationManager requestWhenInUseAuthorization];
+    }
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [locationManager startUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray *)locations {
+    {
+        _detectedLocation = [locations lastObject];
+    }}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError: %@", error);
+    
+}
+
+- (NSString *) getCoordinates{
+    return [NSString stringWithFormat:@"%@,@", _detectedLocation.coordinate.latitude, _detectedLocation.coordinate.longitude];
+}
+
+
+@end
